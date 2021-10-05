1e7a910461ccb59e28b0bab8a24174a717b89c41
SethPavlicek <spavlicek@sfsu.edu>
2021-10-04 17:32:39 -0700

    moved units weapons rituals and recruits to respective files

--- DRG/build.sh
+++ DRG/build.sh
@@ -2,10 +2,17 @@
 set -x # echo on
 # list files in order with a backslash "\" at the end of each line
 cat \
+    misc.c5p\
+    rituals.c5p\
+    CommonWeapons.c5p\
+    Glyphids.c5p\
     Gunner.c5p \
     Engineer.c5p \
     Driller.c5p \
     Scout.c5p \
+    class.c5p\
   > DRG.c5m.final
 set +x # echo off
 echo "Done joining files."
+
+# misc, rituals, weapons (universal), glyphids, commanders (already there), class (last)
